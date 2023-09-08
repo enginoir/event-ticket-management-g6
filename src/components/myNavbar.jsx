25d3f46d086319aee1c3a60442036541a34bece1
enginoir <myusuf141012@gmail.com>
2023-09-08 15:34:45 +0700

    MyNavbar_DropDownMenu optimization for development

--- src/components/myNavbar.jsx
+++ src/components/myNavbar.jsx
@@ -1,73 +1,115 @@
 import React from "react";
-import { NavbarBrand, NavbarText, Navbar, Nav, NavItem, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem } from "reactstrap";
+import {
+  NavbarBrand,
+  NavbarText,
+  Navbar,
+  Nav,
+  NavItem,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from "reactstrap";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { logoutUser } from "../redux/actions/user";
 
-function myNavbar(props) {
-    return (
-        <div>
-            <Navbar color="light" light>
-                <NavbarBrand>Doodoos Events Management</NavbarBrand>
-                <Nav>
-                    {
-                        props.userGlobal.username ?
-                            <>
-                                <NavItem style={{ marginTop: 9 }}>
-                                    <NavbarText >Hello, {props.userGlobal.username}</NavbarText>
-                                </NavItem>
-                                <UncontrolledDropdown nav inNavbar>
-                                    <DropdownToggle nav caret>
-                                        pages
-                                    </DropdownToggle>
-                                    <DropdownMenu right>
-                                        <DropdownItem>
-                                            <Link to="/search">Search</Link>
-                                        </DropdownItem>
-                                        <DropdownItem>
-                                            <Link to="/register">register</Link>
-                                        </DropdownItem>
-                                        <DropdownItem>
-                                            <Link to="/login">login</Link>
-                                        </DropdownItem>
-                                        {
-                                            props.userGlobal.role === "admin" ?
-                                                <DropdownItem>
-                                                    <Link to="/admin">Admin</Link>
-                                                </DropdownItem>
-                                                : null
-                                        }
+function MyNavbar(props) {
+  const handleLogout = () => {
+    props.logoutUser();
+  };
 
-                                        <DropdownItem divider />
-                                        <DropdownItem onClick={props.logoutUser}>
-                                            <Link>Logout</Link>
-                                        </DropdownItem>
-
-                                    </DropdownMenu>
-                                </UncontrolledDropdown>
-                            </>
-                            :
-                            <NavItem>
-                                <NavbarText>
-                                    <Link to='/login'> Login </Link> | <Link to='/register'> Register </Link>
-                                </NavbarText>
-                            </NavItem>
-                    }
-
-                </Nav>
-            </Navbar>
+  return (
+    <div>
+      <Navbar color="light" light>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            width: "100%",
+          }}
+        >
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <NavbarBrand>Doodoos Events Management</NavbarBrand>
+            <Nav>
+              {props.userGlobal.username ? (
+                <>
+                  <NavItem style={{ marginTop: 9 }}>
+                    <NavbarText>
+                      Hello, my friend{" "}
+                      <span style={{ color: "blue" }}>
+                        {props.userGlobal.username}
+                      </span>
+                    </NavbarText>
+                  </NavItem>
+                  <UncontrolledDropdown nav inNavbar>
+                    <DropdownToggle nav caret>
+                      <i className="fas fa-bars"></i> {}
+                    </DropdownToggle>
+                    <DropdownMenu right>
+                      {props.userGlobal.role === "admin" ? (
+                        <DropdownItem>
+                          <Link to="/admin">Admin</Link>
+                        </DropdownItem>
+                      ) : (
+                        <>
+                          <DropdownItem>
+                            <Link to="/profile">Profile</Link>
+                          </DropdownItem>
+                          <DropdownItem>
+                            <Link to="/carts">Carts</Link>
+                          </DropdownItem>
+                          <DropdownItem>
+                            <Link to="/account-settings">Account Settings</Link>
+                          </DropdownItem>
+                        </>
+                      )}
+                      <DropdownItem divider />
+                      {}
+                    </DropdownMenu>
+                  </UncontrolledDropdown>
+                </>
+              ) : (
+                <NavItem style={{ marginLeft: "auto" }}>
+                  <NavbarText>
+                    <Link to="/login"> Login </Link> |{" "}
+                    <Link to="/register"> Register </Link>
+                  </NavbarText>
+                </NavItem>
+              )}
+            </Nav>
+          </div>
+          {props.userGlobal.username ? (
+            <Link to="/" style={{ textDecoration: "none" }}>
+              <button
+                style={{
+                  backgroundColor: "red",
+                  color: "white",
+                  padding: "10px 20px",
+                  borderRadius: "5px",
+                  cursor: "pointer",
+                }}
+                onClick={handleLogout}
+              >
+                Log Out
+              </button>
+            </Link>
+          ) : null}
         </div>
-    )
+      </Navbar>
+    </div>
+  );
 }
 
 const mapStateToProps = (state) => {
-    return {
-        userGlobal: state.user
-    }
-}
+  return {
+    userGlobal: state.user,
+  };
+};
 
-const mapDispatchToProps= {
-    logoutUser,
-}
+const mapDispatchToProps = {
+  logoutUser,
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(myNavbar)
+export default connect(mapStateToProps, mapDispatchToProps)(MyNavbar);
