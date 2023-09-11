32fe615b6df64cea8a0c9dc8652f3eb9d5f72dff
Evelio Excellenta <velioexcel@gmail.com>
2023-09-11 20:02:06 +0700

    modified:   src/components/Comment.jsx

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,79 +1,75 @@
-import React from "react";
-import "../assets/styles/ProductCard.css";
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux'
-import Axios from "axios"
-import { API_URL } from "../constants/API";
-import { getCartData } from '../redux/actions/cart'
+import React, { useState } from 'react';
+import {
+    Box,
+    Text,
+    Input,
+    Button,
+    VStack,
+    StackDivider,
+    Avatar,
+    Flex,
+    Spacer,
+} from '@chakra-ui/react';
+import { API_URL } from '../constants/API';
+import { connect } from "react-redux";
+import ReactStars from "react-rating-stars-component";
 
-function ProductCard(props) {
-    
-    const addToCartHandler = () => {
-        // Check apakah user sudah memiliki barang tsb di cart
-        Axios.get(`${API_URL}/carts`, {
-            params: {
-                userId: props.userGlobal.id,
-                productId: props.productData.id,
-            }
-        })
-        .then((result) => {
-            if (result.data.length) {
-                Axios.patch(`${API_URL}/carts/${result.data[0].id}`, {
-                    quantity: result.data[0].quantity + 1
-                })
-                .then(() => {
-                    alert("Berhasil menambahkan barang");
-                    props.getCartData(props.userGlobal.id)
-                })
-                .catch(() => {
-                    alert("Terjadi kesalahan di server");
-                })
-            } else {
-                Axios.post(`${API_URL}/carts`, {
-                    userId: props.userGlobal.id,
-                    productId: props.productData.id,
-                    price: props.productData.price,
-                    productName: props.productData.productName,
-                    productImage: props.productData.productImage,
-                    quantity: 1,
-                })
-                .then(() => {
-                    alert("Berhasil menambahkan barang");
-                    props.getCartData(props.userGlobal.id)
-                })
-                .catch(() => {
-                    alert("Terjadi kesalahan di server");
-                });
-            };
-        })
-    }
+
+function CommentSection(props) {
+
+    const [comments, setComments] = useState([]);
+    const [newComment, setNewComment] = useState('');
+
+    const handleAddComment = () => {
+        if (newComment) {
+            setComments([...comments, newComment]);
+            setNewComment('');
+        }
+    };
+
+    const ratingChanged = (newRating) => {
+        console.log(newRating);
+    };
 
     return (
-        <div className="card product-card">
-            <img src={props.productData.productImage} alt="" />
-            <div className="mt-1">
-                <div>
-                    <Link to={`/product-detail/${props.productData.id}`}>
-                        <h4>{props.productData.productName}</h4>
-                    </Link>
-                    <span className="text-muted">Rp. {props.productData.price}</span>
-                </div>
-                <div className="d-flex flex-row justify-content-end">
-                    <button onClick={addToCartHandler} className="btn btn-primary mt-2">Add to Cart</button>
-                </div>
+        <VStack spacing={4} divider={<StackDivider borderColor="gray.200" />}>
+            <ReactStars
+                count={5}
+                onChange={ratingChanged}
+                size={24}
+                activeColor="#ffd700" 
+                classNames={"mt-3"}
+            />
+            <Text fontSize="xl" fontWeight="bold">
+                Comments
+            </Text>
+            {comments.map((comment, index) => (
+                <Flex key={index} align="start" justifyContent="flex-start">
+                    <Avatar size="sm" name={props.userGlobal.username} />
+                    <Text ml={2}>{comment}</Text>
+                    <Spacer />
+                </Flex>
+            ))}
+
+            <Input
+                placeholder="Add a comment..."
+                value={newComment}
+                onChange={(e) => setNewComment(e.target.value)}
+            />
+            <div className='d-inline-flex' style={{ justifyContent: 'space-between' }}>
+                <Button colorScheme="blue" onClick={handleAddComment}>
+                    Add Comment
+                </Button>
             </div>
-        </div>
+
+        </VStack>
     );
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
     return {
         userGlobal: state.user,
-    }
-}
+    };
+};
 
-const mapDispatchToProps = {
-    getCartData,
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(ProductCard)
+export default connect(mapStateToProps)(CommentSection);
