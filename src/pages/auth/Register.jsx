640f02f8f32822a33af1aa45c10fd50ef47177ba
Evelio Excellenta <velioexcel@gmail.com>
2023-09-11 11:30:04 +0700

    modified:   src/pages/auth/Register.jsx

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react"
 import { registerUser, checkEmail, checkReferral } from "../../redux/actions/user"
-// import { checkEmail } from "../../redux/actions/user"
 import { connect } from "react-redux"
 import { Button } from "@chakra-ui/react"
 import { Alert, AlertIcon, Input, Container, Heading, Stack, Text, HStack, Link } from "@chakra-ui/react"
@@ -22,7 +21,6 @@ function Register(props) {
 
     const inputHandler = (key, value) => {
         setState({ ...state, [key]: value });
-        // console.log(state);
     };
 
     const [alert, setAlert] = useState({ show: false, message: "" });
@@ -148,39 +146,3 @@ const mapDispatchToProps = {
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Register);
-
-
-
-// export default connect (mapStateToProps, mapDispatchToProps)
-
-
-// }
-
-
-// <div className="container">
-                // <div className="row">
-                //     <div className="col-12 text-center">
-                //         <h1>Register Now</h1>
-                //         <p className="lead">
-                //             Register now and start shopping
-                //         </p>
-                //     </div>
-                // </div>
-                // <div className="row mt-5">
-                //     <div className="col-4 offset-4">
-                //         <div className="card">
-                //             <div className="card-body">
-                //                 <h5 className="font-weight-bold mb-3">Register</h5>
-                //                 <input onChange={this.inputHandler} placeholder="Full Name" type="text"  />
-                //                 <input onChange={this.inputHandler} placeholder="Username" type="text"  />
-                //                 <input onChange={this.inputHandler} placeholder="Email" type="text"  />
-                //                 <input onChange={this.inputHandler} placeholder="Password" type="password"  />
-                //                 <div className="d-flex flex-row justify-content-between align-items-center">
-                //                     <button className="btn btn-primary mt-2"> Register</button>
-                //                     <Link to="/login">Or login</Link>
-                //                 </div>
-                //             </div>
-                //         </div>
-                //     </div>
-                // </div>
-            // </div>
